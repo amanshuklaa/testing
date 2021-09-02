743fd108589d53dc0f0940bf132507411ced8a3c
Aman Shukla <amanshukla@Amans-MacBook-Pro.local>
2021-09-02 11:36:36 +0530

    added new file for python

---
+++ Untitled4.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+def evenodd(num):
+    if num%2 == 0:
+        print("number iseven")
+    else:
+        print("numberis odd")
+
+asdfjasdflsadf;ljds
+
+
+evenodd(5)
+
+
+
+
